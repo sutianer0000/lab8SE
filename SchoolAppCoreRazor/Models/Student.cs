c64636047132a8c7797a61da446d274c5728fb46
thinh <sutianer0000@gmail.com>
2026-04-08 17:54:13 +0700

    Initial commit for Exercise 1

---
+++ SchoolAppCoreRazor/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SchoolAppCoreRazor.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public required string LastName
+        {
+            get; set;
+        }
+        public required string FirstName
+        {
+            get; set;
+        }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment>? Enrollments
+        { get; set; }
+    }
+}
+
---
+++ SchoolAppCoreRazor/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SchoolAppCoreRazor.Models;
+
+namespace SchoolAppCoreRazor.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SchoolAppCoreRazor.Models.SchoolContext _context;
+
+        public CreateModel(SchoolAppCoreRazor.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+
+            ViewData["DepartmentID"] = new SelectList(_context.Departments.Select(d => new { d.DepartmentID, DisplayText = d.DepartmentID + " - " + d.DepartmentName }), "DepartmentID", "DisplayText");
+
+
+            return Page();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
